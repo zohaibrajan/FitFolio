05014855e6b714e905a40dddae242cc01e75c1b2
Zohaib <zohaibrajan@gmail.com>
2023-11-29 18:34:02 -0500

    WeightExercise Model Complete

--- app/models/weight_exercise.py
+++ app/models/weight_exercise.py
@@ -1,25 +1,24 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 
-class CardioExercise(db.Model):
-    __tablename__ = "cardio_exercises"
+class WeightExercise(db.Model):
+    __tablename__ = "weight_exercises"
 
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
     exercise_name = db.Column(db.String(50), nullable=False)
-    sets = db.Column(db.Integer, nullable=False)
-    repetitions = db.Column(db.Integer, nullable=False)
-    weight_per_rep = db.Column(db.Integer, nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+    calories_burned = db.Column(db.Integer, nullable=False)
     created_by_user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("user.id")), nullable=True)
+
 
     def to_dict(self):
         return {
             "id": self.id,
             "exerciseName": self.exercise_name,
-            "sets": self.sets,
-            "repetitions": self.repetitions,
-            "weightPerRep": self.weight_per_rep,
+            "duration": self.duration,
+            "caloriesBurned": self.calories_burned,
             "createdByUserId": self.created_by_user_id
         }
