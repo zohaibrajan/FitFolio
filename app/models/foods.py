ee1e5901aafc8642bd227bd923e6c1de8d63613a
Zohaib <zohaibrajan@gmail.com>
2023-11-29 19:28:55 -0500

    Goals Model Complete, began DailySummary Model

--- app/models/foods.py
+++ app/models/foods.py
@@ -1,23 +1,25 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 
-
-class WeightExercise(db.Model):
-    __tablename__ = "weight_exercises"
+class Food(db.Model):
+    __tablename__ = "foods"
 
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
+
     id = db.Column(db.Integer, primary_key=True)
-    exercise_name = db.Column(db.String(50), nullable=False)
-    duration = db.Column(db.Integer, nullable=False)
-    calories_burned = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    restaurant = db.Column(db.String(50), nullable=False)
+    calories = db.Column(db.Integer, nullable=False)
+    protein = db.Column(db.Integer, nullable=False)
     created_by_user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("user.id")), nullable=True)
 
     def to_dict(self):
         return {
             "id": self.id,
-            "exerciseName": self.exercise_name,
-            "duration": self.duration,
-            "caloriesBurned": self.calories_burned,
+            "name": self.name,
+            "restaurant": self.restaurant,
+            "calories": self.calories,
+            "protein": self.protein,
             "createdByUserId": self.created_by_user_id
         }
---
+++ app/models/goals.py
@@ -0,0 +1,27 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+
+class Goal(db.Model):
+    __tablename__ = "goals"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("user.id")), nullable=False)
+    goal = db.Column(db.String(25), nullable=False)
+    current_weight = db.Column(db.Integer)
+    starting_weight = db.Column(db.Integer, nullable=False)
+    target_weight = db.Column(db.Integer, nullable=False)
+    lbs_per_week = db.Column(db.Integer, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "userId": self.user_id,
+            "goal": self.goal,
+            "currentWeight": self.current_weight,
+            "startingWeight": self.starting_weight,
+            "targetWeight": self.target_weight,
+            "lbsPerWeek": self.lbs_per_week
+        }
