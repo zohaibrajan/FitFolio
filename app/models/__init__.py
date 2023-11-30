9de7de1b585c26d434cd0d0a32a5898a8a0336ed
Zohaib <zohaibrajan@gmail.com>
2023-11-30 13:07:58 -0500

    Migrated all Models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,11 @@
 from .db import db
 from .user import User
+from .cardio_exercises import CardioExercise
+from .cardio_log import CardioLog
+from .day import Day
+from .food_log import FoodLog
+from .foods import Food
+from .goals import Goal
+from .weight_exercise import WeightExercise
+from .weight_log import WeightLog
 from .db import environment, SCHEMA
