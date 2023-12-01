2ec65dec269e964dcc3dc1d1828171bdd2fcec78
Zohaib <zohaibrajan@gmail.com>
2023-11-30 19:35:39 -0500

    Began working on Weight Exercises Routes

---
+++ app/api/weight_exercise_routes.py
@@ -0,0 +1,16 @@
+from flask import Blueprint
+from flask_login import login_required
+from app.models import WeightExercise
+
+
+weight_exercise_routes = Blueprint("weight-exercise", __name__)
+
+@weight_exercise_routes.route("")
+@login_required
+def get_all_weight_exercises():
+    """"Getting all Weight Exercises"""
+    exercises = WeightExercise.query.all()
+
+    return {
+        "weightExercises": [exercise.to_dict() for exercise in exercises]
+    }
