fb1ea04ac4232906c0190319ccb165cee6ef3517
Zohaib <zohaibrajan@gmail.com>
2023-12-03 15:39:45 -0500

    Landing Page Complete

---
+++ react-app/src/components/LandingPage/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import './LandingPage.css'
+import { useHistory } from "react-router-dom";
+
+
+function LandingPage() {
+    const history = useHistory()
+
+    function onClick(e) {
+        e.preventDefault()
+
+        history.replace('/signup')
+    }
+
+    return (
+      <section className="landing-page-section">
+        <div className="landing-page">
+          <div className="landing-page-text">
+            <span style={{ opacity: "0.6" }}>A nutrition tracking app</span>
+            <h1 className="landing-page-title">
+              <span style={{ fontSize: "65px" }}>Reach your goals</span>
+              <span style={{ fontWeight: "400", fontSize: "65px" }}>
+                with FitFolio
+              </span>
+            </h1>
+            <span style={{ opacity: "0.8", marginTop: "10px" }}>
+              Build healthy habits with the all-in-one food, exercise, and
+              calorie tracker
+            </span>
+            <button onClick={onClick} id="get-started-button">
+              START TODAY <i class="fa-solid fa-angle-right"></i>
+            </button>
+          </div>
+        </div>
+      </section>
+    );
+}
+
+export default LandingPage
--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,34 +1,38 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
 	const sessionUser = useSelector(state => state.session.user);
 
 	return (
-		<ul className='nav-bar-list'>
-			<li>
-				<NavLink id='nav-link-to-home' exact to="/">
-					<p id='project-title'>
-						fitfolio
-					</p>
-				</NavLink>
-			</li>
-			{isLoaded && (
-				<div className='nav-login-signup'>
-					<li>
-						<NavLink to="/login">Login</NavLink>
-					</li>
-					<hr aria-orientation='vertical' style={{fontSize: "0px", opacity: "0.4"}}/>
-					<li>
-						<NavLink to="/signup">Sign Up</NavLink>
-					</li>
-				</div>
-			)}
-		</ul>
-	);
+    <ul className="nav-bar-list">
+      <li>
+        <NavLink id="nav-link-to-home" exact to="/">
+          <p id="project-title">fitfolio</p>
+        </NavLink>
+      </li>
+      {isLoaded && (
+        <div className="nav-login-signup">
+          <li>
+            <NavLink className="login-signup-link" to="/login">
+              Login
+            </NavLink>
+          </li>
+          <hr
+            aria-orientation="vertical"
+            style={{ fontSize: "0px", opacity: "0.4" }}
+          />
+          <li>
+            <NavLink className="login-signup-link" to="/signup">
+              Sign Up
+            </NavLink>
+          </li>
+        </div>
+      )}
+    </ul>
+  );
 }
 
 export default Navigation;
