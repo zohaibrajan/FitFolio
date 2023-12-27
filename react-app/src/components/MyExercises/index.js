68a8bb26ae1d141ea560dfd51073b60a6a6a0907
Zohaib <zohaibrajan@gmail.com>
2023-12-27 16:54:05 -0500

    Editing custom exercise

--- react-app/src/components/MyExercises/index.js
+++ react-app/src/components/MyExercises/index.js
@@ -7,10 +7,15 @@ import OpenModalButton from "../OpenModalButton";
 import CardioLogModal from "../CardioLogModel";
 import WeightLogModal from "../WeightLogModal";
 import { useDispatch, useSelector } from "react-redux";
+import "./MyExercises.css";
 
 function MyExercises({ exerciseType }) {
   exerciseType = exerciseType.charAt(0).toUpperCase() + exerciseType.slice(1);
   const dispatch = useDispatch();
+  const [isPanelOpen, setIsPanelOpen] = useState(false);
+  const [duration, setDuration] = useState(60);
+  const [selectedExercise, setSelectedExercise] = useState({});
+  const [caloriesBurned, setCaloriesBurned] = useState(selectedExercise?.caloriesPerMinute * duration);
   const userExercisesObj = useSelector((state) => state.userExercises);
   const userExercises = Object.values(userExercisesObj);
 
@@ -21,6 +26,10 @@ function MyExercises({ exerciseType }) {
       dispatch(getUsersWeightExercisesThunk());
     }
   }, [dispatch, exerciseType]);
+
+
+  console.log(selectedExercise)
+  console.log(caloriesBurned)
 
   return (
     <>
@@ -35,13 +44,27 @@ function MyExercises({ exerciseType }) {
                 <OpenModalButton
                   modalComponent={
                     exerciseType === "Cardio" ? (
-                      <CardioLogModal exerciseName={exercise.exerciseName} exerciseId={exercise.id}/>
+                      <CardioLogModal
+                        exerciseName={exercise.exerciseName}
+                        exerciseId={exercise.id}
+                      />
                     ) : (
-                      <WeightLogModal exerciseName={exercise.exerciseName} exerciseIdProp={exercise.id}/>
+                      <WeightLogModal
+                        exerciseName={exercise.exerciseName}
+                        exerciseIdProp={exercise.id}
+                      />
                     )
                   }
                   buttonText={"Add to Diary"}
                 />
+                <button
+                  onClick={() => {
+                    setIsPanelOpen(true);
+                    setSelectedExercise(exercise);
+                  }}
+                >
+                  Edit Exercise
+                </button>
               </div>
             ))
           ) : (
@@ -50,6 +73,52 @@ function MyExercises({ exerciseType }) {
             </div>
           )}
         </div>
+        {isPanelOpen && (
+          <div className={`side-panel ${isPanelOpen ? "" : "hidden"}`}>
+            <h1>Edit Exercise</h1>
+            <form>
+              <label>
+                Exercise Name:
+                <input
+                  type="text"
+                  name="exerciseName"
+                  value={selectedExercise.exerciseName}
+                  onChange={(e) => {
+                    setSelectedExercise({
+                      ...selectedExercise,
+                      exerciseName: e.target.value,
+                    });
+                  }}
+                />
+              </label>
+              <label>
+                Exercise Type:
+                <select
+                  type="text"
+                  name="exerciseType"
+                  value={selectedExercise.exerciseType}
+                >
+                  <option value="cardio">Cardio</option>
+                  <option value="strength">Strength</option>
+                </select>
+              </label>
+              <label>
+                Duration:
+                <input
+                  type="number"
+                  value={duration}
+                  name="exerciseDuration"
+                  onChange={(e) => setDuration(e.target.value)}
+                ></input>
+              </label>
+              <label>
+                Calories Burned:
+                <input type="number" value={caloriesBurned} onChange={(e) => setCaloriesBurned(e.target.value)}></input>
+              </label>
+            </form>
+            <button onClick={() => setIsPanelOpen(false)}>Close</button>
+          </div>
+        )}
       </div>
     </>
   );
