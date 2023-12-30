c68eb675fce8e8c7069a94084e9602c599eb33ec
Zohaib <zohaibrajan@gmail.com>
2023-12-30 17:05:22 -0500

    404 page added

---
+++ react-app/src/components/PageNotFound/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./PageNotFound.css";
+
+
+function PageNotFound() {
+    return (
+        <div className="page-not-found-container">
+            <div className="page-not-found-text">
+                <h1>404 Page Not Found</h1>
+            </div>
+        </div>
+    );
+    }
+
+export default PageNotFound;
