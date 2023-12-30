82be7e0e0375881fc343c2cde6d5a9b8a04d14aa
Zohaib <zohaibrajan@gmail.com>
2023-12-30 15:34:23 -0500

    Added pagination to Exercise section

--- react-app/src/components/EditExercisePanel/index.js
+++ react-app/src/components/EditExercisePanel/index.js
@@ -167,6 +167,7 @@ function EditExercisePanel({
     <div className={`side-panel`}>
       <div className="edit-exercise-panel-title-container">
         <span>Edit Exercise</span>
+        <i onClick={() => setIsPanelOpen(false)} className="fa-solid fa-xmark close-panel"></i>
       </div>
       <form
         className="edit-exercise-form"
