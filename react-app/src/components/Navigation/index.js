30b943e078fed1dd4c056ed468beecd9d31b7eb0
Zohaib <zohaibrajan@gmail.com>
2024-01-03 19:40:45 -0500

    Added Follow section to navigation

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -58,6 +58,35 @@ function Navigation({ isLoaded }) {
               Sign Up
             </NavLink>
           </li>
+          <hr
+            aria-orientation="vertical"
+            style={{ fontSize: "0px", opacity: "0.4" }}
+          />
+          <span
+            className="follow-us"
+          >
+            Follow Us:
+          </span>
+          <a
+            target="_blank"
+            href="https://github.com/zohaibrajan"
+            rel="noreferrer"
+          >
+            <i
+              className="fa-brands fa-square-github"
+              style={{ color: "#000000", fontSize: "18px", cursor: "pointer" }}
+            ></i>
+          </a>
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/zohaib-rajan-718198216/"
+            rel="noreferrer"
+          >
+            <i
+              className="fa-brands fa-linkedin"
+              style={{ color: "#0077b5", fontSize: "18px", cursor: "pointer" }}
+            ></i>
+          </a>
         </div>
       ) : (
         <div className="logged-in-nav-bar">
