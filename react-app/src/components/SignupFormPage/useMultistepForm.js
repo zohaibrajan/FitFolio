6357e982712882e5262c02bf1a5199823f196140
Zohaib <zohaibrajan@gmail.com>
2024-04-12 12:33:23 -0400

    Implemeted custom react hook to create a mutli step sign up form

---
+++ react-app/src/components/SignupFormPage/useMultistepForm.js
@@ -0,0 +1,32 @@
+export function useMultistepForm(steps) {
+    const [currentStepIndex, setCurrentStepIndex] = useState(0);
+
+    function next() {
+        setCurrentStepIndex(i => {
+            if (i >= steps.length - 1) return i;
+            return i + 1;
+        })
+    }
+
+    function back() {
+        setCurrentStepIndex(i => {
+            if (i <= 0) return i;
+            return i - 1;
+        })
+    }
+
+    function goTo(i) {
+        setCurrentStepIndex(i);
+    }
+
+    return {
+        currentStepIndex,
+        step: steps[currentStepIndex],
+        steps,
+        next,
+        back,
+        goTo,
+        isLastStep: currentStepIndex === steps.length - 1,
+        isFirstStep: currentStepIndex === 0,
+    }
+}
