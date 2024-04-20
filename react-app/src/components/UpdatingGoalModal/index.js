78d5bfcfdc1991014e1c18836b0f922397c8d85a
Zohaib <zohaibrajan@gmail.com>
2024-04-20 02:56:20 -0400

    UpdateGoalModal styled and ready

--- react-app/src/components/UpdatingGoalModal/index.js
+++ react-app/src/components/UpdatingGoalModal/index.js
@@ -1,27 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { useMultistepForm } from "../SignupFormPage/useMultistepForm";
 import { updateGoalThunk } from "../../store/goal";
 import { useModal } from "../../context/Modal";
 import { AllGoals } from "./UpdatingGoalSteps";
+import { validateWeeklyGoal, validateWeights } from "../../utils";
 import {
   WeeklyGoal,
   GetCurrentAndTargetWeight,
 } from "../SignupFormPage/SignupFormSteps";
+import "./UpdatingGoalModal.css";
 
 function UpdatingGoalModal() {
   const dispatch = useDispatch();
-  const { closeModal } = useModal();
   const history = useHistory();
+  const { closeModal } = useModal();
   const [error, setError] = useState("");
   const goal = useSelector((state) => state.goal);
   const DATA = {
     goal: goal.goal,
     currentWeight: goal.startingWeight,
     targetWeight: goal.targetWeight,
+    // Weekly goal is intentionally left blank to be filled in by user
     weeklyGoal: "",
-  };
+  }
   const updateData = (fields) => {
     setData((prev) => ({ ...prev, ...fields }));
   };
@@ -33,10 +36,21 @@ function UpdatingGoalModal() {
       <GetCurrentAndTargetWeight {...data} updateData={updateData} />,
     ]);
 
+  useEffect(() => {
+    setError("");
+  }, [currentStepIndex]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const validators = [null, validateWeeklyGoal, validateWeights];
+    const validator = validators[currentStepIndex];
+    if (validator && validator(data)) {
+      setError(validator(data));
+      return;
+    }
+
     if (!isLastStep) return next();
-    console.log(data);
 
     let { goal, currentWeight, targetWeight, weeklyGoal } = data;
     if (goal === "Maintain Weight") {
@@ -61,14 +75,21 @@ function UpdatingGoalModal() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div className="update-goal-form-parent">
+      <form className="update-goal-form-container" onSubmit={handleSubmit}>
         {step}
-        <div>
-          <button type="button" onClick={isFirstStep ? closeModal : back}>
+        {error && <div id="error-message">{error}</div>}
+        <div className="update-goal-buttons-container">
+          <button
+            id="update-goal-form-back"
+            type="button"
+            onClick={isFirstStep ? closeModal : back}
+          >
             BACK
           </button>
-          <button type="submit">{isLastStep ? "CONFIRM" : "NEXT"}</button>
+          <button id="update-goal-form-next" type="submit">
+            {isLastStep ? "CONFIRM" : "NEXT"}
+          </button>
         </div>
       </form>
     </div>
