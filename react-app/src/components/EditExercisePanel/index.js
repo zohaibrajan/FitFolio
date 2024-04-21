694dbf8d7dc617100b657192c3c8144d77e09158
Zohaib <zohaibrajan@gmail.com>
2024-04-21 00:13:19 -0400

    Created custom react hooks to get data from state

--- react-app/src/components/EditExercisePanel/index.js
+++ react-app/src/components/EditExercisePanel/index.js
@@ -1,9 +1,46 @@
+import React, { useState, useEffect } from "react";
 import EditCardioExercise from "./EditCardioExercise";
 import { FormInput } from "../FormElements";
 import ErrorHandlingComponent from "../ErrorHandlingComponent";
+import { useDispatch } from "react-redux";
+import { getAllCardioExercisesThunk } from "../../store/cardioExercises";
+import { getAllWeightExercisesThunk } from "../../store/weightExercises";
+import {
+  checkForExercise,
+  useCardioExercises,
+  useWeightExercises,
+  useUserExercises,
+} from "../../utils";
 import "./EditExercisePanel.css";
 
 function EditExercisePanel({ selectedExercise, isCardio, setIsPanelOpen }) {
+  const [exerciseName, setExerciseName] = useState(
+    selectedExercise.exerciseName.split("*")[0]
+  );
+  const [nameError, setNameError] = useState("");
+  const dispatch = useDispatch();
+  const usersExercises = useUserExercises();
+  const cardioExercises = useCardioExercises();
+  const weightExercises = useWeightExercises();
+
+  console.log(usersExercises);
+
+  useEffect(() => {
+    // useEffect to get exercises based on exerciseType
+    const thunkAction = isCardio
+      ? getAllCardioExercisesThunk
+      : getAllWeightExercisesThunk;
+    dispatch(thunkAction());
+    checkForExercise(
+      exerciseName,
+      isCardio ? "Cardio" : "Strength",
+      cardioExercises,
+      weightExercises,
+      usersExercises,
+      setNameError
+    ); // checks for exercise when exerciseType changes
+    // eslint-disable-next-line
+  }, [dispatch, isCardio]);
   return (
     <div className={`side-panel`}>
       <div className="edit-exercise-panel-title-container">
@@ -17,9 +54,12 @@ function EditExercisePanel({ selectedExercise, isCardio, setIsPanelOpen }) {
         <FormInput
           label={"Exercise Name"}
           type={"text"}
-          value={selectedExercise.name}
-          placeholder={"Exercise Name eg. Running"}
+          value={exerciseName}
           name={"exercise-name"}
+          onChange={(e) => {
+            setExerciseName(e.target.value);
+            setNameError("");
+          }}
         />
         <ErrorHandlingComponent error={false} />
         {isCardio ? (
--- react-app/src/components/ExercisePage/index.js
+++ react-app/src/components/ExercisePage/index.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ErrorHandlingComponent from "../ErrorHandlingComponent"; // absolute import
 import StrengthExerciseForm from "./StrengthPage";
 import CardioForm from "./CardioPage";
 import MyExercises from "../MyExercises";
 import { FormInput, FormSelect } from "../FormElements";
-import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllCardioExercisesThunk } from "../../store/cardioExercises";
 import { getAllWeightExercisesThunk } from "../../store/weightExercises";
